c2bf0e82b15c5d1222fbbd40f41378c8be1396a0
Dmitry Savkin <dima.savkin.2002@mail.ru>
2025-06-06 18:43:59 +0300

    Switch to Server-Side Rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,49 @@
-import { serve } from "bun";
+import { serve, build, file } from "bun";
+import { renderToReadableStream } from "react-dom/server";
 import index from "./index.html";
+import App from "./app";
+
+await build({
+  entrypoints: [
+    "./src/main.tsx",
+    "./src/styles.css",
+  ],
+  outdir: "./public",
+});
 
 const server = serve({
   routes: {
-    "/": index,
+    "/": async () =>
+      new Response(
+        await renderToReadableStream(<Page />, {
+          bootstrapModules: ["/main.js"],
+        })
+      ),
   },
-
+  fetch(req) {
+  	const pathname = new URL(req.url).pathname;
+    return new Response(file(`./public${pathname}`));
+  },
   development: process.env.NODE_ENV !== "production" && {
     hmr: true,
     console: true,
   },
 });
+
+function Page() {
+  return (
+    <html>
+      <head>
+        <title>ToDo List</title>
+        <link rel="stylesheet" href="/styles.css" />
+      </head>
+      <body>
+        <div id="root">
+          <App />
+        </div>
+      </body>
+    </html>
+  );
+}
 
 console.log(`Server running at ${server.url}`);
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import App from "./app";
 
-const elem = document.getElementById("root")!;
-const app = (
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
-if (import.meta.hot) {
-  const root = (import.meta.hot.data.root ??= createRoot(elem));
-  root.render(app);
-} else {
-  createRoot(elem).render(app);
-}
+const root = document.getElementById("root");
+hydrateRoot(root, <App />);
